95537c52b99442f0d258d59ccf6a511fc5894371
Stefan Haack <shaack@gmail.com>
2020-06-08 20:15:03 +0200

    Fixed import of chess.js

--- postinstall.js
+++ postinstall.js
@@ -6,5 +6,6 @@
 const LibraryManager = require("cm-web-modules/src/LibraryManager.js")
 const manager = new LibraryManager(__dirname)
 
-manager.addProject("cm-web-modules")
 manager.addProject("chess.js", "", "chess.js")
+manager.addProject("cm-web-modules")
+manager.addProject("cm-pgn")
