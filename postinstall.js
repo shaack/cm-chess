f7cfe4d7dce59dbbef8269dc03d687bea96c3b82
Stefan Haack <shaack@gmail.com>
2020-06-08 19:45:03 +0200

    for now, only a proxy es6 module for chess.js

---
+++ postinstall.js
@@ -0,0 +1,10 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * License: MIT, see file 'LICENSE'
+ */
+
+const LibraryManager = require("cm-web-modules/src/LibraryManager.js")
+const manager = new LibraryManager(__dirname)
+
+manager.addProject("cm-web-modules")
+manager.addProject("chess.js", "", "chess.js")
