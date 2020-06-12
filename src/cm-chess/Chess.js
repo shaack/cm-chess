088e05f641b681679f54e133645a568fcd53ab14
Stefan Haack <shaack@gmail.com>
2020-06-12 02:48:34 +0200

    FEN declaration in constructor

--- src/cm-chess/Chess.js
+++ src/cm-chess/Chess.js
@@ -10,13 +10,12 @@
  */
 class CmChess extends Chess {
 
-    static FEN = {
-        empty: "8/8/8/8/8/8/8/8 w - - 0 1",
-        start: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-    }
-
     constructor(fen) {
         super(fen)
+        this.FEN = {
+            empty: "8/8/8/8/8/8/8/8 w - - 0 1",
+            start: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        }
         // this.state.pgn = new Pgn() ToDo
     }
 
