9798460f9882cd740c2122e2e82812c31520c1f6
shaack <shaack@gmail.com>
2022-06-29 00:05:19 +0200

    developed `Fen.js`

--- src/cm-chess/Fen.js
+++ src/cm-chess/Fen.js
@@ -7,29 +7,26 @@
 export class Fen {
 
     constructor(fen) {
-        this.fen = fen
-    }
-
-    get fen() {
-        return this._fen
+        this.parse(fen)
     }
 
     /**
      * https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
      * @param fen
      */
-    set fen(fen) {
-        this._fen = fen
+    parse(fen) {
         const fenParts = fen.split(" ")
         this.position = fenParts[0]
         this.colorToPlay = fenParts[1]
         this.castlings = fenParts[2].split("")
         this.enPassantTargetSquare = fenParts[3]
-        if(this.enPassantTargetSquare === "-") {
-            this.enPassantTargetSquare = undefined
-        }
         this.plyClock = parseInt(fenParts[4], 10)
         this.moveNumber = parseInt(fenParts[5], 10)
+    }
+
+    toString() {
+        return this.position + " " + this.colorToPlay + " " + this.castlings.join("") + " " +
+            this.enPassantTargetSquare + " " + this.plyClock + " " + this.moveNumber
     }
 
 }
--- test/TestFen.js
+++ test/TestFen.js
@@ -4,12 +4,14 @@ import {Fen} from "../src/cm-chess/Fen.js"
 describe("Fen", function () {
 
     it("should parse a fen", () => {
-        const fen = new Fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2")
+        const fenString = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
+        const fen = new Fen(fenString)
         assert.equal(fen.position, "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R")
         assert.equal(fen.colorToPlay, "b")
         assert.equal(fen.castlings.length, 4)
-        assert.equal(fen.enPassantTargetSquare, undefined)
+        assert.equal(fen.enPassantTargetSquare, "-")
         assert.equal(fen.plyClock, 1)
         assert.equal(fen.moveNumber, 2)
+        assert.equal(fen.toString(), fenString)
     })
 })
